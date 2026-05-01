6f69ed0f1d28e1feac9b7940eb9633f863ebce00
Vashu Kumar <kashyapvasu2468@gmail.com>
2026-05-01 21:40:17 +0530

    Implement Lazy Load

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,104 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import { lazy, Suspense } from 'react';
 
 // Components
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx'; 
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+
 
 // Pages
-import Home from './pages/Home.jsx';
-import Blog from './pages/Blog.jsx';
-import Contact from './pages/Contact.jsx';
-import Shop from './pages/Shop.jsx';
-import BookPage from './pages/Book/BookPage.jsx';
-import Dashboard from './pages/Dashboard/Dashboard.jsx';
-import MyReservation from './pages/MyReservation/MyReservation.jsx';
-import MyLoans from './pages/MyLoans/MyLoans.jsx';
-import UserLayout from './pages/UserLayout/UserLayout.jsx';
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Blog = lazy(() => import('./pages/Blog.jsx'));
+const Contact = lazy(() => import('./pages/Contact.jsx'));
+const Shop = lazy(() => import('./pages/Shop.jsx'));
+const BookPage = lazy(() => import('./pages/Book/BookPage.jsx'));
+const MyReservation = lazy(() => import('./pages/MyReservation/MyReservation.jsx'));
+const MyLoans = lazy(() => import('./pages/MyLoans/MyLoans.jsx'));
+const UserLayout = lazy(() => import('./pages/UserLayout/UserLayout.jsx'));
+const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard.jsx'))
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
-   
-      <main>
-        {/* HEADER */}
-        <Header 
-          searchQuery={searchQuery} 
-          setSearchQuery={setSearchQuery} 
-        />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
+    <main>
+      {/* HEADER */}
+      <Header
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+        <Suspense fallback={<div>Loading...</div>}>
 
-          <Route 
-            path="/shop" 
-            element={<Shop searchQuery={searchQuery} />} 
-          />
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/shop"
+              element={<Shop searchQuery={searchQuery} />}
+            />
 
-          {/*  Protected Routes */}
-          <Route 
-            path="/book" 
-            element={
-              <ProtectedRoute>
-                <BookPage />
-              </ProtectedRoute>
-            } 
-          />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
 
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            } 
-          />
+            {/*  Protected Routes */}
+            <Route
+              path="/book"
+              element={
+                <ProtectedRoute>
+                  <BookPage />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route 
-            path="/my-reservations" 
-            element={
-              <ProtectedRoute>
-                <MyReservation />
-              </ProtectedRoute>
-            } 
-          />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route 
-            path="/my-loans" 
-            element={
-              <ProtectedRoute>
-                <MyLoans />
-              </ProtectedRoute>
-            } 
-          />
+            <Route
+              path="/my-reservations"
+              element={
+                <ProtectedRoute>
+                  <MyReservation />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route 
-            path="/user" 
-            element={
-              <ProtectedRoute>
-                <UserLayout />
-              </ProtectedRoute>
-            } 
-          />
+            <Route
+              path="/my-loans"
+              element={
+                <ProtectedRoute>
+                  <MyLoans />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route path="*" element={<Home />} />
-        </Routes>
+            <Route
+              path="/user"
+              element={
+                <ProtectedRoute>
+                  <UserLayout />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Suspense>
 
         {/* FOOTER */}
         <Footer />
 
-      </main>
-    
+    </main>
+
   );
 }
 
