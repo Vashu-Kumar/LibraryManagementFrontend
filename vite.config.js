444b7086fa4e771bd17e253c021231a4cdfdb2f5
Vashu Kumar <kashyapvasu2468@gmail.com>
2026-05-01 16:28:34 +0530

    some improvements in vite.config.js so bundle size drops down during deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          animations: ['gsap'],
+          swiper: ['swiper'],
+          icons: ['react-icons'],
+          http: ['axios'],
+        }
+      }
+    }
+  }
 })
-
